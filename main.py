8567c38661bad2a6513b66343341bcf2430f9bfa
fuji <ramenm1001@gmail.com>
2025-07-05 16:12:53 +1000

    start

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 pygame.init()
 
+
 class Player:
     def __init__(self, x, y, width=15, height=40, color=(185, 122, 87)):
         self.rect = pygame.Rect(x, y, width, height)
@@ -10,7 +11,8 @@ class Player:
         self.speed_y = 0
 
     def draw(self):
-        pygame.draw.rect(win, self.color, self.rect )
+        pygame.draw.rect(win, self.color, self.rect)
+
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
@@ -19,22 +21,6 @@ class Player:
             self.rect = self.rect.move(-5, 0)
 
         moving = self.rect.move(0, 1)
-        all_platforms = platforms.copy()
-        if platforms_draw:
-            all_platforms.extend(platforms_draw)
-        for platform in all_platforms:
-            if platform.rect.colliderect(moving):
-                self.rect.bottom = platform.rect.top
-                return
-        self.is_fall = True
-        if self.speed_y < 5:
-            self.speed_y += 1
-        self.rect = self.rect.move(0, self.speed_y)
-        for platform in all_platforms:
-            if platform.rect.colliderect(moving):
-                self.rect.bottom = platform.rect.top
-                return
-
 
     def update(self):
         self.move()
@@ -44,6 +30,8 @@ class Player:
 class Bullet:
     def __init__(self, x, y, x1, y1):
         self.rect = pygame.Rect(x, y, 1, 1)
+
+
 class Enemy:
     def __init__(self, x, y, first_dot, second_dot, width=20, height=20, color=(254, 0, 0)):
         self.rect = pygame.Rect(x, y, width, height)
@@ -90,21 +78,15 @@ def get_angle(x, y, x1, y1):
 win = pygame.display.set_mode((500, 500))
 run = True
 player = Player(50, 0)
-platforms = [Platforms(50, 80, 150),
-             Platforms(100, 100)]
 enemy = Enemy(80, 40, (50, 40), (450, 40))
 
-platforms_draw = []
 drawing = False
 erase = False
-paint = 700
+hp = 100
 font = pygame.font.Font(None, 32)
 text = font.render(f"краска str(paint)",
                    False,
                    (255, 255, 255))
-
-paint = 820
-
 
 while run:
     mouse = pygame.mouse.get_pos()
@@ -123,28 +105,12 @@ while run:
             if eve.button == 3:
                 erase = False
     mouse = pygame.mouse.get_pos() # (150, 125)
-    if drawing and paint > 0:
-        plat = Platforms(mouse[0], mouse[1])
-        screen = pygame.Rect(0, 0, 500, 500)
-        if screen.colliderect(plat.rect):
-            platforms_draw.append(plat)
-            paint -= 1
 
-    if erase:
-        eraser = pygame.Rect(*mouse, 10, 10)
-        for platform in platforms_draw:
-            if platform.rect.colliderect(eraser):
-                platforms_draw.remove(platform)
-                paint += 1
-    text = font.render(f"краска {str(paint)}",
+    text = font.render(f"краска {str(hp)}",
                        True,
                        (255, 255, 255))
 
     win.fill((0, 0, 0))
-    for platform in platforms:
-        platform.update()
-    for platform in platforms_draw:
-        platform.update()
     player.update()
     enemy.update()
     win.blit(text, (10, 10))
